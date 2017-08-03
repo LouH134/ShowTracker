e860aa69f4be8e7fff6be791110a372abbf29d85
Louis <Lharris134@gmail.com>
2017-08-03 14:31:24 -0400

    beginning project

---
+++ ShowTracker/ShowTracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ShowTracker
+//
+//  Created by Louis Harris on 8/3/17.
+//  Copyright © 2017 Louis Harris. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
